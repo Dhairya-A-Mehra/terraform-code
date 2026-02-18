2b6334a96b5e22addd34dbc7c902c374eaedc3d8
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2026-02-18 14:44:48 +0530

    Enhance DynamoDB table configuration with additional attributes and GSI

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -3,16 +3,35 @@ resource "aws_dynamodb_table" "this" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = var.hash_key
 
+  # Primary key attribute
   attribute {
     name = var.hash_key
     type = "S"
   }
 
-  tags = {
-    Environment = "demo"
+  # Additional attributes for GSI
+  attribute {
+    name = "product_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "N"
+  }
+
+  # Global Secondary Index
+  global_secondary_index {
+    name            = "product-created-index"
+    hash_key        = "product_id"
+    range_key       = "created_at"
+    projection_type = "ALL"
   }
 
   stream_enabled   = true
   stream_view_type = "NEW_IMAGE"
-}
 
+  tags = {
+    Environment = "demo"
+  }
+}
